81797fe9914828922a6fd37959bb4354dcf67a48
Raveendra Soori <sooriravindra@gmail.com>
2023-04-08 08:33:43 -0700

    Add git-gutter highlighting

--- emacs/.emacs.rsoori/init.el
+++ emacs/.emacs.rsoori/init.el
@@ -118,5 +118,17 @@
 (use-package default-text-scale
   :config (default-text-scale-mode 1))
 
+;; Git gutter
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
 ;; Load custom configuration
 (load-file custom-file)
