81a236545545030f0b49cc68db12ece9324842de
Raveendra Soori <sooriravindra@gmail.com>
2023-04-09 18:21:49 -0700

    Adding evil undo system

--- emacs/.emacs.rsoori/init.el
+++ emacs/.emacs.rsoori/init.el
@@ -52,6 +52,7 @@
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  (evil-set-undo-system 'undo-redo)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
